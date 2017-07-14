01f9a32d32bbaba03dd9e47a61690f3929da52c8
Michael Reynald <michaelreynald78@gmail.com>
2017-07-14 16:37:06 +0700

    Initial commit

---
+++ dbdump/2017_wsc2017_php_framework_v1.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Community v12.4.0 (64 bit)
+MySQL - 5.7.17-log : Database - 2017_wsc2017_php_framework_v1
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`2017_wsc2017_php_framework_v1` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `2017_wsc2017_php_framework_v1`;
+
+/*Table structure for table `posts` */
+
+DROP TABLE IF EXISTS `posts`;
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `context` longtext,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+/*Data for the table `posts` */
+
+insert  into `posts`(`id`,`name`,`context`) values 
+(6,'test sfsd','fsdf fdsf');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
